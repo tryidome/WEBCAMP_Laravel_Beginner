de11312f5417936ca5c8b683eae5d4200323f5e1
tryidome <113878350+tryidome@users.noreply.github.com>
2023-01-09 01:48:42 +0000

    非ログイントップページ作成

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_type=1);
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class AuthController extends controller
+{
+     public function index()
+    {
+        return view('index');
+    }
+    
+}
+
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\AuthController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,5 +20,10 @@ use App\Http\Controllers\WelcomeController;
 //     return view('welcome');
 // });
 
+// テスト用
 Route::get('/welcome', [WelcomeController::class, 'index']);
 Route::get('/welcome/second', [WelcomeController::class, 'second']);
+
+// タスク管理システム
+Route::get('/', [AuthController::class, 'index']);
+
